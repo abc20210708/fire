dc5aacfa8842784cffb96700b76688db6f788d1a
real <hb743054@gmail.com>
2021-11-23 09:04:37 +0900

    로그인 설정

---
+++ wow-master/src/main/java/com/spring/mvc/member/service/MemberService.java
@@ -0,0 +1,33 @@
+package com.spring.mvc.member.service;
+
+import com.spring.mvc.member.domain.Member;
+import com.spring.mvc.member.repository.MemberMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private  final MemberMapper memberMapper;
+
+    //아이디 중복 확인 처리
+    public boolean isDuplicate(String checkId) {
+        return memberMapper.isDuplicate(checkId) == 1;
+    }
+
+    //이메일 중복확인 중간처리
+    public boolean isDuplicate2(String checkEmail) {
+        return memberMapper.isDuplicate2(checkEmail) == 1;
+    }
+
+    public void signUp(Member member) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodePw = encoder.encode(member.getPassword());
+        member.setPassword(encodePw);
+
+        memberMapper.register(member);
+    }
+
+}//end class
